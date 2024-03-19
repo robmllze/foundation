2cef9ed4cc5e158845ffb096fcee672218ee948b
Robert Mollentze <86869208+robmllze@users.noreply.github.com>
2024-03-19 20:06:07 +1100

    Update setup_foundation.zsh

--- setup_foundation.zsh
+++ setup_foundation.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-git clone https://github.com/robmllze/___generators.git
-git clone https://github.com/robmllze/_data-foundation.git _data
-git clone https://github.com/robmllze/_service_interfaces-foundation.git _service_interfaces
-git clone https://github.com/robmllze/_services-foundation.git _services
-git clone https://github.com/robmllze/_view-foundation.git _view
+git clone -main https://github.com/robmllze/___generators.git
+git clone -main https://github.com/robmllze/_data-foundation.git _data
+git clone -main https://github.com/robmllze/_service_interfaces-foundation.git _service_interfaces
+git clone -main https://github.com/robmllze/_view-foundation.git _view
+
+git clone -with_firebase https://github.com/robmllze/_services-foundation.git _services
 
 cd ___generators && flutter pub get && cd ..
 cd _data && flutter pub get && cd ..
