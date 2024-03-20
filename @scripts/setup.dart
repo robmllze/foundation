06fd53bf37cc2b6f37537bd84850b6e4b7587781
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-20 22:53:16 +1100

    Update

--- @scripts/setup.dart
+++ @scripts/setup.dart
@@ -20,13 +20,14 @@ void main(List<String> args) async {
   final name = "foundation";
   await Future.wait([
     $("git clone -b main ${src}foundation.git $name"),
-    $("git submodule update --init --recursive", name),
+    //$("git submodule update --init --recursive", name),
     // $("git clone -b main ${src}___generators.git ___generators", name),
     // $("git clone -b main ${src}_data-foundation.git _data", name),
     // $("git clone -b main ${src}_service_interfaces-foundation.git _service_interfaces", name),
     // $("git clone -b main ${src}_view-foundation.git _view", name),
-    $("git clone -b $servicesBranchName ${src}_services-foundation.git _services", name),
+    //$("git clone -b $servicesBranchName ${src}_services-foundation.git _services", name),
   ]);
+  await $("git submodule update --init --recursive", name);
   await Future.wait([
     $("dart pub get -C ___generators", name),
     $("dart pub get -C _data", name),
