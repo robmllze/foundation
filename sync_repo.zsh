bdb684e4617d4fef087360b85960f3b318ecdf46
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-21 16:07:12 +1100

    Update

---
+++ sync_repo.zsh
@@ -0,0 +1,20 @@
+##.title
+## ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+##
+## 🇽🇾🇿 & Dev
+##
+## Copyright Ⓒ Robert Mollentze, xyzand.dev
+## 
+## Licensing details can be found in the LICENSE file in the root directory.
+## 
+## ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+##.title~
+
+# Sync this folder with https://github.com/robmllze/foundation.
+git init
+git add .
+git commit -m "upstream"
+git remote remove upstream
+git remote add upstream https://github.com/robmllze/foundation.git
+git fetch upstream
+git merge upstream/main --allow-unrelated-histories --no-edit
